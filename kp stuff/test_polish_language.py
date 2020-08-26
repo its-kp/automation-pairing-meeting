396e2ab2a2f45833b8f9a79676d7ec0404c5eac3
Trent Hammer <thammer@ostusa.com>
2020-08-26 15:18:22 -0500

    We found Polish

--- kp stuff/test_polish_language.py
+++ kp stuff/test_polish_language.py
@@ -8,5 +8,10 @@ browser.get("https://translate.google.com/")
 dropdown_button = "/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]"
 browser.find_element_by_xpath(dropdown_button).click()
 # Grab all languages from the dropdown
-
+find_language = '//div[@class="language_list_item language_list_item_language_name"]'
+languages = browser.find_elements_by_xpath(find_language)
+# print(languages)
 # Verify Polish appears in the dropdown
+for language_search in languages:
+    if 'Polish' == language_search.text:
+       print('POLISH!!')
