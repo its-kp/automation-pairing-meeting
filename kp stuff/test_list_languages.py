71ed2089a13bf2a2fa45cc5b380bdf4bc7b8c923
Doua Xiong <dxiong.gnoixd@gmail.com>
2020-08-27 15:56:32 -0500

    Updated locators, updated comparison if statement.

--- kp stuff/test_list_languages.py
+++ kp stuff/test_list_languages.py
@@ -1,5 +1,7 @@
 # Import webdriver
 from selenium import webdriver
+import time
+
 browser = webdriver.Chrome()
 
 # Navigate to translate.google.com
@@ -8,23 +10,32 @@ browser.get('https://translate.google.com/')
 # Grab list of languages from source dropdown
 dropdown_source_button = '/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]'
 browser.find_element_by_xpath(dropdown_source_button).click()
-find_language = '//div[@class="language_list_item language_list_item_language_name"]'
-source_language = browser.find_elements_by_xpath(find_language)
+time.sleep(1)
+source_locator = '//div[@class="language-list"][1]//div[@class="language_list_item language_list_item_language_name"]'
+source_element_list = browser.find_elements_by_xpath(source_locator)
 source_language_list = list()
-for each in source_language:
+for each in source_element_list:
     source_language_list.append(each.text)
 
 # Grab list of languages from target dropdown
 dropdown_target_button = '/html/body/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/div[3]'
+browser.find_element_by_xpath(dropdown_target_button).click()
+browser.find_element_by_xpath(dropdown_target_button).click()
 browser.find_elements_by_xpath(dropdown_target_button)
-translate_language = '//div[@class="language_list_item language_list_item_language_name"]'
-translate_target = browser.find_elements_by_xpath(translate_language)
-translate_target_list = list()
-for each in translate_target:
-    translate_target_list.append(each.text)
+time.sleep(1)
+target_locator = '//div[@class="language-list"][2]//div[@class="language_list_item language_list_item_language_name"]'
+target_element_list = browser.find_elements_by_xpath(target_locator)
+target_language_list = list()
+for each in target_element_list:
+    target_language_list.append(each.text)
 
 # Verify that all languages in both lists are present.
-if source_language_list == translate_target_list:
+if source_language_list == target_language_list:
     print("These are the same.")
-    print(translate_target_list)
-    print(source_language_list)
+else:
+    print("NOT THE SAME.")
+
+print("This is the source list:", source_language_list)
+print("This is that target list:", target_language_list)
+
+browser.quit()
